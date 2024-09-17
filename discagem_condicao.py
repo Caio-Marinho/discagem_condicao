97ae2f75a906be6727c9890bcad57400e7cb83f5
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-17 16:37:22 -0300

    versão final

--- discagem_condicao.py
+++ discagem_condicao.py
@@ -12,22 +12,26 @@ while menu != 6:
                      "6 para sair\n"
                      "Digite: "))  #Escolher a opção
 
-match menu:
-    case 1:  #soma
-        soma = numero1 + numero2
-        print(f"A soma dos valores {numero1} + {numero2} = {soma}")
-    case 2:  #subtração
-        subtracao = numero1 - numero2
-        print(f"A subtração dos valores {numero1} - {numero2} = {subtracao}")
-    case 3:  #multiplicação
-        multiplicacao = numero1 * numero2
-        print(f"A multiplicação dos valores {numero1} X {numero2} = {multiplicacao}")
-    case 4:  # divisão
-        while numero2 == 0:  #verifica se não é uma divisão por 0
-            numero2 = int(input("Não é possível divisão por 0\n"
-                                "Informe um novo valor para o 2º Número: "))
-        divisao = numero1 / numero2
-        print(f"A divisão dos valores {numero1} / {numero2} = {divisao}")
-    case 5:  #repete para inserir um novo número
-        print(end='')
-        novo = 'um novo valor para'
+    match menu:
+        case 1:  #soma
+            soma = numero1 + numero2
+            print(f"A soma dos valores {numero1} + {numero2} = {soma}")
+        case 2:  #subtração
+            subtracao = numero1 - numero2
+            print(f"A subtração dos valores {numero1} - {numero2} = {subtracao}")
+        case 3:  #multiplicação
+            multiplicacao = numero1 * numero2
+            print(f"A multiplicação dos valores {numero1} X {numero2} = {multiplicacao}")
+        case 4:  # divisão
+            while numero2 == 0:  #verifica se não é uma divisão por 0
+                numero2 = int(input("Não é possível divisão por 0\n"
+                                    "Informe um novo valor para o 2º Número: "))
+            divisao = numero1 / numero2
+            print(f"A divisão dos valores {numero1} / {numero2} = {divisao}")
+        case 5:  #repete para inserir um novo número
+            print(end='')
+            novo = 'um novo valor para'
+    if menu != 5:
+        print("Começando novamente...")
+else:  #é chamado caso 6 seja selecionado
+    print("Obrigado pela sua atenção!!")
