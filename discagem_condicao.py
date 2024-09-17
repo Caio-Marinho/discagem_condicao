a6cfb5e5813393d0ccc3fb3a576c4c9fb00f97df
Caio-Marinho <kaigabriel12@gmail.com>
2024-09-17 16:25:47 -0300

    versão final

--- discagem_condicao.py
+++ discagem_condicao.py
@@ -1,8 +1,8 @@
 menu = 5
 novo = ''
 while menu != 6:
-    numero1 = float(input(f"Digite {novo} o 1º Número: "))
-    numero2 = float(input(f"Digite {novo} o 2º Número: "))
+    numero1 = float(input(f"Digite {novo} o 1º Número: "))  #Inserir o primeiro numero
+    numero2 = float(input(f"Digite {novo} o 2º Número: "))  #Inserir o segundo numero
     menu = int(input("informe a opção\n"
                      "1 para soma\n"
                      "2 para subtração\n"
@@ -10,25 +10,26 @@ while menu != 6:
                      "4 para divisão\n"
                      "5 digitar um novo numero\n"
                      "6 para sair\n"
-                     "Digite: "))
+                     "Digite: "))  #Escolher a opção
+
     match menu:
-        case 1:
+        case 1:  #soma
             soma = numero1 + numero2
             print(f"A soma dos valores {numero1} + {numero2} = {soma}")
-        case 2:
+        case 2:  #subtração
             subtracao = numero1 - numero2
             print(f"A subtração dos valores {numero1} - {numero2} = {subtracao}")
-        case 3:
+        case 3:  #multiplicação
             multiplicacao = numero1 * numero2
             print(f"A multiplicação dos valores {numero1} X {numero2} = {multiplicacao}")
-        case 4:
-            if numero2 == 0:
+        case 4:  # divisão
+            while numero2 == 0:  #verifica se não é uma divisão por 0
                 numero2 = int(input("Não é possível divisão por 0\n"
                                     "Informe um novo valor para o 2º Número: "))
             divisao = numero1 / numero2
             print(f"A divisão dos valores {numero1} / {numero2} = {divisao}")
-        case 5:
+        case 5: #repete para inserir um novo número
             print(end='')
             novo = 'um novo valor para'
-else:
+else: #é chamado caso 6 seja selecionado
     print("Obrigado pela sua atenção!!")
